b1179a3b568a8ced40c99fb811e9233b1bdd4641
Mapache <javichujar03@gmail.com>
2026-06-30 13:00:46 +0200

    broken fixes

--- resis/app/layout.tsx
+++ resis/app/layout.tsx
@@ -1,6 +1,6 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter, Geist } from "next/font/google";
-import "./globals.css";
 import Navbar from "./components/Navbar";
 import { ThemeProvider } from "./components/theme-provider";
 import { cn } from "@/lib/utils";
